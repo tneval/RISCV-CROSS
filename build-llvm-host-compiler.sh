c2fc841cdca433d1da59d493ac1e46d368355901
Tapio Nevalainen <tapio.nevalainen@tuni.fi>
2026-05-29 15:47:58 +0300

    build the host compiler with shared libraries

--- build-llvm-host-compiler.sh
+++ build-llvm-host-compiler.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# Builds LLVM that acts as a 'host' compiler when cross-compiling pocl.
+# Builds LLVM that acts as a 'host' compiler for pocl in the context of cross-compilation.
 # Used to build kernel library.
 # Has to support RISC-V target.
+
+
+# When cross-compiling PoCL, two or three compilers are needed depending on the setup:
+# 1. Actual cross-compiler that produces the RISC-V native PoCL libraries. (e.g. riscv64-linux-gnu-gcc/g++)
+# 2. PoCL 'host' compiler that has to be Clang. This has to be x86 binary and have RISC-V target.
+#    This will compile the kernel library (LLVM bitcode), but cmake tests that it supports RISC-V target.
+# 3. RISC-V native LLVM that PoCL get linked to.
 
 LLVM_TAG="llvmorg-22.1.6"
 LLVM_SPIRV_BRANCH="llvm_release_220"
@@ -23,7 +30,6 @@ if [ ! -d "sources/llvm-project" ]; then
 
     git clone https://github.com/KhronosGroup/SPIRV-LLVM-Translator.git sources/llvm-project/llvm/projects/SPIRV-LLVM-Translator
     git -C sources/llvm-project/llvm/projects/SPIRV-LLVM-Translator checkout ${LLVM_SPIRV_BRANCH}
-    exit 1
 
 else
     echo "repository already exists, skipping clone"
@@ -37,6 +43,8 @@ if [ ! -d "build/${D_NAME}" ]; then
         -DLLVM_ENABLE_ASSERTIONS=ON \
         -DLLVM_TARGETS_TO_BUILD="X86;RISCV" \
         -DLLVM_ENABLE_PROJECTS="clang" \
+        -DLLVM_BUILD_LLVM_DYLIB=ON \
+        -DLLVM_LINK_LLVM_DYLIB=ON \
         sources/llvm-project/llvm
         (cd build/${D_NAME}/ && ninja install)
 else
@@ -44,6 +52,7 @@ else
 fi
 
 
+# In case this will be used on another machine.
 if [ ! -f "tarballs/${D_NAME}_${LLVM_TAG}.tar.gz" ]; then
     tar -czf "tarballs/${D_NAME}_${LLVM_TAG}.tar.gz" "install/${D_NAME}"
 fi
--- remote-sysroot-method/invoke_cmake.sh
+++ remote-sysroot-method/invoke_cmake.sh
@@ -1,26 +1,69 @@
 #!/bin/bash
 
-BUILD_DIR=build-remote-riscv
-SOURCE_DIR=/home/tapio/vault/repositories/pocl-unpublished-wts/loopvec-next
+# Variables:
+###########################################################
+# Choose PoCL repository
+POCL_REPO="git@github.com:pocl/unpublished.git"
 
 # the root filesystem of the board
-TARGET_ROOT=/home/tapio/SSHFS-mountpoint/neptune
+# Local or over remote?
+TARGET_ROOT=/home/tapio/SSHFS-mountpoint/neptune/
+
 # the location of Target LLVM inside the TARGET_ROOT
-TARGET_LLVM_INSTALL_DIR=/opt/LLVM_22_SPIRV
+TARGET_LLVM_INSTALL_DIR=/opt/LLVM_22_SPIRV/lib/cmake/llvm
+
+TARGET_LLVM_LIB_RPATH=/opt/LLVM_22_SPIRV/lib
+
+###########################################################
+
+BASE_DIR="$(pwd)/../"
+
+SOURCE_DIR=${BASE_DIR}/sources/pocl-unpublished-wts/
+
+INSTALL_DIR=${BASE_DIR}/install/pocl-test
+
 # host-side LLVM
-HOST_LLVM_CONFIG=/home/tapio/vault/Software/LLVM-22-Release/bin/llvm-config
+HOST_LLVM_CONFIG="$(pwd)/../install/llvm-x86-with-riscv-target/bin/llvm-config"
 
 # Target settings
 TARGET_TRIPLE=riscv64-unknown-linux-gnu
 TARGET_CPU=spacemit-x60
 
-# set location
-TARGET_TOOLCHAIN_FILE=/home/tapio/vault/repositories/RISCV-CROSS/remote-sysroot-method/cross_milkv.cmake
+TARGET_TOOLCHAIN_FILE="$(pwd)/cross_milkv.cmake"
 
 
-cmake -DCMAKE_MAKE_PROGRAM=/usr/bin/make \
-      -DLLVM_DIR=${TARGET_ROOT}${TARGET_LLVM_INSTALL_DIR} -DWITH_LLVM_CONFIG=${HOST_LLVM_CONFIG} \
-      -DHOST_DEVICE_BUILD_HASH=${TARGET_TRIPLE} -DCMAKE_TOOLCHAIN_FILE=${TARGET_TOOLCHAIN_FILE}  \
-      -DLLC_TRIPLE=${TARGET_TRIPLE} -DLLC_HOST_CPU=${TARGET_CPU} -DCMAKE_PREFIX_PATH=${TARGET_ROOT} \
-      -DENABLE_ICD=ON -DENABLE_LOADABLE_DRIVERS=0 -DKERNELLIB_HOST_CPU_VARIANTS=${TARGET_CPU} \
-      -B ${BUILD_DIR} ${SOURCE_DIR}
+mkdir -p "${SOURCE_DIR}"
+
+# Controls branch in PoCL repo
+BRANCH=$1
+
+case "$BRANCH" in
+      # Loopvec-next
+      lvn)
+            export WORKTREE_DIR="${SOURCE_DIR}/loopvec-next"
+
+            if [ ! -d "${WORKTREE_DIR}" ]; then
+                  git clone ${POCL_REPO} "${WORKTREE_DIR}"
+            fi
+
+            export BUILD_DIR="${WORKTREE_DIR}/build"
+            export INSTALL_DIR="${BUILD_DIR}/install"
+            ;;
+      *)
+            echo "Branch not found"
+            exit 0
+            ;;
+esac
+
+if [ ! -d ${BUILD_DIR} ]; then
+      cmake -G Ninja -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DCMAKE_INSTALL_RPATH=${TARGET_LLVM_LIB_RPATH} \
+            -DLLVM_DIR=${TARGET_ROOT}${TARGET_LLVM_INSTALL_DIR} -DWITH_LLVM_CONFIG=${HOST_LLVM_CONFIG} \
+            -DHOST_DEVICE_BUILD_HASH=${TARGET_TRIPLE} -DCMAKE_TOOLCHAIN_FILE=${TARGET_TOOLCHAIN_FILE} \
+            -DLLC_TRIPLE=${TARGET_TRIPLE} -DLLC_HOST_CPU=${TARGET_CPU} -DCMAKE_PREFIX_PATH=${TARGET_ROOT} \
+            -DENABLE_ICD=ON -DENABLE_LOADABLE_DRIVERS=0 -DKERNELLIB_HOST_CPU_VARIANTS=${TARGET_CPU} \
+            -B ${BUILD_DIR} ${WORKTREE_DIR}
+fi
+
+(cd ${BUILD_DIR} && ninja install)
+
+tar -czf "${BASE_DIR}/tarballs/PoCL-RISCV-${BRANCH}.tar.gz" ${INSTALL_DIR}
